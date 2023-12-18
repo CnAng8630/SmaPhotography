87580c861a78a173bc6ce070cd0683993de21f6a
CnAng8630 <138589979+CnAng8630@users.noreply.github.com>
2023-12-18 15:58:52 +0000

    Add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
+  scope module: :public do
+    root to: 'homes#top'
+    get 'about' => 'homes#about'
+  end
+
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
