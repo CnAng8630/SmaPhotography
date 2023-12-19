820b2d3c7ddd5d6c7066d2f09316d9ec0843633a
CnAng8630 <138589979+CnAng8630@users.noreply.github.com>
2023-12-19 10:41:49 +0000

    Add sign_up

---
+++ app/controllers/public/post_photos_controller.rb
@@ -0,0 +1,10 @@
+class Public::PostPhotosController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  scope module: :public do
-    root to: 'homes#top'
-    get 'about' => 'homes#about'
-  end
-
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
@@ -12,5 +7,14 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
+
+  scope module: :public do
+    root to: 'homes#top'
+    get 'about' => 'homes#about'
+
+    resources :post_photos, only: [:new, :create, :index, :show, :destroy]
+
+    resources :users, only: [:index, :show, :edit, :update]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
